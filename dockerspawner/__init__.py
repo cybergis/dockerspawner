dca72b0707dc67bcdec4f853b38b226a3ec4dfff
zhiyuli <zyli2004@gmail.com>
2021-01-26 15:40:10 -0600

    init

--- dockerspawner/__init__.py
+++ dockerspawner/__init__.py
@@ -2,5 +2,8 @@ from ._version import __version__
 from .dockerspawner import DockerSpawner
 from .swarmspawner import SwarmSpawner
 from .systemuserspawner import SystemUserSpawner
+from .dockerspawner_cybergis import DockerSpawner_CyberGIS
+from .swarmspawner_cybergis import SwarmSpawner_CyberGIS
 
-__all__ = ['__version__', 'DockerSpawner', 'SwarmSpawner', 'SystemUserSpawner']
+__all__ = ['__version__', 'DockerSpawner', 'SwarmSpawner', 'SystemUserSpawner', 'DockerSpawner_CyberGIS',
+           'SwarmSpawner_CyberGIS']
